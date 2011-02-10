2b0c9a102f1e1cbdcfa222ebd9dce2503e2a1a00
Tonyx <tonyx@yahoo.com>
2011-02-10 02:51:53 +0100

    mock by mockit instead of custom impl

--- src/test/org/externalLangProvider/GoogleApi94Test.java
+++ src/test/org/externalLangProvider/GoogleApi94Test.java
@@ -5,14 +5,24 @@ import org.externalLangProvider.MyContentProvider;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentMatcher;
 import org.tonyxzt.language.core.GenericDictionary;
 import org.tonyxzt.language.core.Translator;
 import org.tonyxzt.language.io.InMemoryOutStream;
 import org.tonyxzt.language.io.InputStream;
+import org.tonyxzt.language.io.OutStream;
+import org.tonyxzt.language.util.BrowserActivator;
 import org.tonyxzt.language.util.FakeBrowserActivator;
 
+import java.awt.geom.Area;
 import java.util.HashMap;
 import java.util.Map;
+
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.argThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by IntelliJ IDEA.
@@ -23,13 +33,14 @@ import java.util.Map;
  */
 public class GoogleApi94Test {
     Map<String,GenericDictionary> mapDictionaries;
-    InMemoryOutStream ios;
+    OutStream ios;
     Translator translator;
-    FakeBrowserActivator fakeBrowserActivator;
+    BrowserActivator fakeBrowserActivator;
     @Before
     public void SetUp() {
-        ios= new InMemoryOutStream();
-        fakeBrowserActivator= new FakeBrowserActivator();
+        ios= mock(OutStream.class);
+        fakeBrowserActivator = mock(BrowserActivator.class);
+
         mapDictionaries = new HashMap<String,GenericDictionary>(){
                {
                     put("myDic",new GenericDictionary("myDic",new MyContentProvider(),new MyContentFilter()));
@@ -41,81 +52,83 @@ public class GoogleApi94Test {
 
     @Test
     public void orilanItalianTargetEnSayHi() throws Exception {
-        InMemoryOutStream ios = new InMemoryOutStream();
-        translator.setOutStream(ios);
         translator.setCommand(new String[]{"--dic=myDic","--oriLang=it","--targetLang=en","ciao"});
         translator.doAction();
-        org.junit.Assert.assertTrue(ios.getContent().contains("hello"));
+
+        verify(ios).output("ciao = hello");
      }
 
 
     @Test
     public void shouldBeAbleToManageAnyInputStreamer() throws Exception {
-        translator.setOutStream(ios);
         translator.setCommand(new String[]{"--dic=myDic", "--oriLang=en", "--targetLang=fr", "hi"});
 
         translator.doAction();
 
-        Assert.assertTrue(ios.getContent().contains("salut"));
+        verify(ios).output("hi = salut");
     }
 
 
     @Test
-    public void canReadFromInputFileMultipleLines() throws Exception {
-        InputStream inputStream = new InputStream(){ boolean start = true; public String next() {if (start) { start=false; return "ciao";} else return null;}};
+    public void canReadFromInputStream() throws Exception {
+        InputStream inputStream = mock(InputStream.class);
+        when(inputStream.next()).thenReturn("ciao").thenReturn(null);
+
         translator.setCommand(new String[] {"--dic=myDic", "--oriLang=it","--targetLang=en","--inFile=infile"});
         translator.setInputStream(inputStream);
-        translator.setOutStream(ios);
 
         translator.doAction();
-
-        Assert.assertTrue(ios.getContent().contains("hello"));
+        verify(ios).output("ciao = hello");
     }
 
+
     @Test
-    public void canReadFromInputFile() throws Exception {
+    public void canReadFromStremEnglishToItalian() throws Exception {
+        InputStream inputStream = mock(InputStream.class);
         translator.setCommand(new String[] {"--dic=myDic","--oriLang=en","--targetLang=it","--inFile=infile"});
-        InputStream inputStream = new InputStream(){ boolean start = true; public String next() {if (start) { start=false; return "hi";} else return null;}};
+        when(inputStream.next()).thenReturn("hi").thenReturn(null);
         translator.setInputStream(inputStream);
-        translator.setOutStream(ios);
 
         translator.doAction();
 
-        Assert.assertTrue(ios.getContent().contains("ciao"));
+        verify(ios).output("hi = ciao");
     }
 
-
-    @Test
-    public void manageOutputFile() throws Exception {
-        translator.setCommand(new String[]  {"--dic=myDic","--oriLang=it","--targetLang=en","ciao"});
-        translator.setOutStream(ios);
-        translator.doAction();
-
-        Assert.assertTrue(ios.getContent().contains("hello"));
-    }
-
+//
+//
     @Test
     public void supportLanguagesContainsItalian() throws Exception {
+        ArgumentMatcher<String> containsItalian = new ArgumentMatcher<String>() {
+            @Override
+            public boolean matches(Object object) {
+                return ((String)object).contains("ITALIAN");
+            }
+        };
         translator.setCommand(new String[]  {"--dic=myDic","--languages"});
-        translator.setOutStream(ios);
         translator.doAction();
+        verify(ios).output(argThat(containsItalian));
 
-        Assert.assertTrue(ios.getContent().contains("ITALIAN"));
     }
-
+//
     @Test
     public void testCyrillic() throws Exception {
         translator.setCommand(new String[]{"--dic=myDic","--oriLang=en","--targetLang=ru","hi"});
-        translator.setOutStream(ios);
         translator.doAction();
 
-        Assert.assertTrue(ios.getContent().contains("привет"));
+        verify(ios).output("hi = привет");
      }
 
+    @Test
+    public void activateBrowser() throws Exception {
+        translator.setCommand(new String[]{"--dic=myDic","--info"});
+        translator.doAction();
 
+        verify(fakeBrowserActivator).activateBrowser("http://http://translate.google.com");
 
+     }
 
 }
+
 
 
 
